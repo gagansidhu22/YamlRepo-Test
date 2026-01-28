e458dc2c7604f3678e542fb48d94b427368c75d4
gagansidhu22 <gagandeepkaur15@academic.rrc.ca>
2026-01-28 16:07:19 -0600

    Update goodhello.py

--- goodhello.py
+++ goodhello.py
@@ -1,10 +1,13 @@
 """This module prints greetings using simple functions."""
+
 def hello():
-"""Prints a simple greeting."""
-print("Hello, DevOps learner!")
+    """Prints a simple greeting."""
+    print("Hello, DevOps learner!")
+
 def bye():
-"""Prints a simple farewell."""
-print("Goodbye!")
+    """Prints a simple farewell."""
+    print("Goodbye!")
+
 if __name__ == "__main__":
-hello()
-bye()
+    hello()
+    bye()
